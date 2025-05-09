7e71c3dc63184fb39ba500429f43321fc6649262
berra-dogan <berra.dogan129@gmail.com>
2025-05-09 17:27:06 +0200

    Striped Hash Set

--- PageProcessing/PageProcessing.h
+++ PageProcessing/PageProcessing.h
@@ -12,11 +12,9 @@ class PageProcessing {
 
         PageProcessing(){};
 
-
         std::string fetch_html(const std::string& url);
 
         std::vector<std::string> extract_links(const std::string& html);
-
 
         std::vector<std::string> visit(const std::string& url);
 };
